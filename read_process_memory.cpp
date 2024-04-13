ae9fb5a56afe2564747fb3546936f3edf91e7dc8
alexthesys <alexsuprunov@gmail.com>
2024-04-13 22:33:04 +0200

    Collect process's modules/threads/heap info

--- read_process_memory.cpp
+++ read_process_memory.cpp
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <windows.h>
 #include <psapi.h>
+#include <tlhelp32.h>
+#include <tchar.h>
 #include <assert.h>
 
 #define MAX_BUFFER_SIZE 0x1000
@@ -27,12 +29,18 @@ static const char* page_type[] = {"MEM_IMAGE", "MEM_MAPPED", "MEM_PRIVATE"};
 static const char* page_protect[] = {"PAGE_EXECUTE", "PAGE_EXECUTE_READ", "PAGE_EXECUTE_READWRITE", "PAGE_EXECUTE_WRITECOPY", "PAGE_NOACCESS", "PAGE_READONLY", 
                                     "PAGE_READWRITE", "PAGE_WRITECOPY", "PAGE_TARGETS_INVALID", "UNKNOWN"};
 
-int is_hex(const char *pattern, size_t pattern_len) {
+static int list_processes();
+static int list_process_modules(DWORD dw_pid);
+static int list_process_threads(DWORD dw_owner_pid);
+static int traverse_heap_list(DWORD dw_pid);
+static void print_error(TCHAR const* msg);
+
+inline int is_hex(const char *pattern, size_t pattern_len) {
     return (((pattern_len > 2) && (pattern[pattern_len - 1] == 'h' || pattern[pattern_len - 1] == 'H'))
         || ((pattern_len > 3) && (pattern[0] == '0' && pattern[1] == 'x')));
 }
 
-const char* get_page_state(DWORD state) {
+static const char* get_page_state(DWORD state) {
     const char *result = NULL;
     switch (state) {
     case MEM_COMMIT:
@@ -48,7 +56,7 @@ const char* get_page_state(DWORD state) {
     return result;
 }
 
-void print_page_type(DWORD state) {
+static void print_page_type(DWORD state) {
     printf("Type:");
     if (state == MEM_IMAGE) {
         printf(" %s\n", page_type[0]);
@@ -63,7 +71,7 @@ void print_page_type(DWORD state) {
     }
 }
 
-const char* get_page_protect(DWORD state) {
+static const char* get_page_protect(DWORD state) {
     // lets not comlicate things with other available options for now
     state &= ~(PAGE_GUARD | PAGE_NOCACHE | PAGE_WRITECOMBINE);
 
@@ -129,11 +137,11 @@ static void parse_input(const char* pattern, search_data *data) {
             data->pattern_len -= 2;
         }
         data->pattern_len /= 2;
-        puts("Searching for a hex value...\n");
+        puts("\nSearching for a hex value...\n");
     } else {
         data->type = it_ascii;
         data->pattern = pattern;
-        puts("Searching for an ascii string...\n");
+        puts("\nSearching for an ascii string...\n");
     }
 }
 
@@ -203,13 +211,22 @@ int main() {
     size_t pid_len = -1;
     int stop = 'n';
     int res;
+    int symbol = (int)'n';
+
+    puts("List system's processes? y/n");
+    symbol = getchar();
+    if (symbol == (int)'y' || symbol == (int)'Y') {
+        list_processes();
+        puts("\n\n====================================\n");
+    }
+
     do {
         int use_same_pid = 0;
         if (pid_len != -1) {
             puts("Use same PID? y/n");
             while ((getchar()) != '\n'); // flush stdin
-            const int reuse_pid = getchar();
-            use_same_pid = (reuse_pid == (int)'y' || reuse_pid == (int)'Y');
+            symbol = getchar();
+            use_same_pid = (symbol == (int)'y' || symbol == (int)'Y');
         }
 
         if (!use_same_pid) {
@@ -227,6 +244,32 @@ int main() {
         if (pid_str == end) {
             puts("Invalid PID! Exiting...");
             return 1;
+        }
+
+        if (!use_same_pid) {
+            puts("List process's modules? y/n");
+            while ((getchar()) != '\n'); // flush stdin
+            symbol = getchar();
+            if (symbol == (int)'y' || symbol == (int)'Y') {
+                list_process_modules(pid);
+                puts("\n\n====================================\n");
+            }
+
+            puts("List process's threads? y/n");
+            while ((getchar()) != '\n'); // flush stdin
+            symbol = getchar();
+            if (symbol == (int)'y' || symbol == (int)'Y') {
+                list_process_threads(pid);
+                puts("\n\n====================================\n");
+            }
+
+            puts("Travers process's heap list? y/n");
+            while ((getchar()) != '\n'); // flush stdin
+            symbol = getchar();
+            if (symbol == (int)'y' || symbol == (int)'Y') {
+                traverse_heap_list(pid);
+                puts("\n\n====================================\n");
+            }
         }
 
         puts("Input pattern (hex value or ascii string): ");
@@ -260,10 +303,215 @@ int main() {
 
         CloseHandle(process);
 
-        puts("\n Continue search? y/n");
+        puts("\n====================================\n");
+        puts("\nContinue search? y/n");
         while ((getchar()) != '\n'); // flush stdin
         stop = getchar();
     } while (stop == (int)'y' || stop == (int)'Y');
 
     return 0;
+}
+
+static int list_processes() {
+    HANDLE hProcessSnap;
+    HANDLE hProcess;
+    PROCESSENTRY32 pe32;
+    DWORD dwPriorityClass;
+
+    // Take a snapshot of all processes in the system.
+    hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (hProcessSnap == INVALID_HANDLE_VALUE)
+    {
+        print_error(TEXT("CreateToolhelp32Snapshot (of processes)"));
+        return(FALSE);
+    }
+
+    // Set the size of the structure before using it.
+    pe32.dwSize = sizeof(PROCESSENTRY32);
+
+    // Retrieve information about the first process,
+    // and exit if unsuccessful
+    if (!Process32First(hProcessSnap, &pe32))
+    {
+        print_error(TEXT("Process32First")); // show cause of failure
+        CloseHandle(hProcessSnap);          // clean the snapshot object
+        return(FALSE);
+    }
+
+    // Now walk the snapshot of processes, and
+    // display information about each process in turn
+    do
+    {
+        _tprintf(TEXT("\n\n====================================================="));
+        _tprintf(TEXT("\nPROCESS NAME:  %s"), pe32.szExeFile);
+        _tprintf(TEXT("\n-------------------------------------------------------"));
+
+        // Retrieve the priority class.
+        dwPriorityClass = 0;
+        hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pe32.th32ProcessID);
+        if (hProcess == NULL)
+            print_error(TEXT("OpenProcess"));
+        else
+        {
+            dwPriorityClass = GetPriorityClass(hProcess);
+            if (!dwPriorityClass)
+                print_error(TEXT("GetPriorityClass"));
+            CloseHandle(hProcess);
+        }
+
+        _tprintf(TEXT("\n  Process ID        = 0x%08X"), pe32.th32ProcessID);
+        _tprintf(TEXT("\n  Thread count      = %d"), pe32.cntThreads);
+        _tprintf(TEXT("\n  Parent process ID = 0x%08X"), pe32.th32ParentProcessID);
+        _tprintf(TEXT("\n  Priority base     = %d"), pe32.pcPriClassBase);
+        if (dwPriorityClass)
+            _tprintf(TEXT("\n  Priority class    = %d"), dwPriorityClass);
+
+    } while (Process32Next(hProcessSnap, &pe32));
+
+    CloseHandle(hProcessSnap);
+    return(TRUE);
+}
+
+static int list_process_modules(DWORD dw_pid) {
+    HANDLE hModuleSnap = INVALID_HANDLE_VALUE;
+    MODULEENTRY32 me32;
+
+    // Take a snapshot of all modules in the specified process.
+    hModuleSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, dw_pid);
+    if (hModuleSnap == INVALID_HANDLE_VALUE)
+    {
+        print_error(TEXT("CreateToolhelp32Snapshot (of modules)"));
+        return(FALSE);
+    }
+
+    // Set the size of the structure before using it.
+    me32.dwSize = sizeof(MODULEENTRY32);
+
+    // Retrieve information about the first module,
+    // and exit if unsuccessful
+    if (!Module32First(hModuleSnap, &me32))
+    {
+        print_error(TEXT("Module32First"));  // show cause of failure
+        CloseHandle(hModuleSnap);           // clean the snapshot object
+        return(FALSE);
+    }
+
+    // Now walk the module list of the process,
+    // and display information about each module
+    do
+    {
+        _tprintf(TEXT("\n\n     MODULE NAME:     %s"), me32.szModule);
+        _tprintf(TEXT("\n     Executable     = %s"), me32.szExePath);
+        _tprintf(TEXT("\n     Process ID     = 0x%08X"), me32.th32ProcessID);
+        _tprintf(TEXT("\n     Ref count (g)  = 0x%04X"), me32.GlblcntUsage);
+        _tprintf(TEXT("\n     Ref count (p)  = 0x%04X"), me32.ProccntUsage);
+        _tprintf(TEXT("\n     Base address   = 0x%08X"), (DWORD)me32.modBaseAddr);
+        _tprintf(TEXT("\n     Base size      = %d"), me32.modBaseSize);
+
+    } while (Module32Next(hModuleSnap, &me32));
+
+    CloseHandle(hModuleSnap);
+    return(TRUE);
+}
+
+static int list_process_threads(DWORD dw_owner_pid) {
+    HANDLE hThreadSnap = INVALID_HANDLE_VALUE;
+    THREADENTRY32 te32;
+
+    // Take a snapshot of all running threads  
+    hThreadSnap = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+    if (hThreadSnap == INVALID_HANDLE_VALUE)
+        return(FALSE);
+
+    // Fill in the size of the structure before using it. 
+    te32.dwSize = sizeof(THREADENTRY32);
+
+    // Retrieve information about the first thread,
+    // and exit if unsuccessful
+    if (!Thread32First(hThreadSnap, &te32))
+    {
+        print_error(TEXT("Thread32First")); // show cause of failure
+        CloseHandle(hThreadSnap);          // clean the snapshot object
+        return(FALSE);
+    }
+
+    // Now walk the thread list of the system,
+    // and display information about each thread
+    // associated with the specified process
+    do
+    {
+        if (te32.th32OwnerProcessID == dw_owner_pid)
+        {
+            _tprintf(TEXT("\n\n     THREAD ID      = 0x%08X"), te32.th32ThreadID);
+            _tprintf(TEXT("\n     Base priority  = %d"), te32.tpBasePri);
+            _tprintf(TEXT("\n     Delta priority = %d"), te32.tpDeltaPri);
+            _tprintf(TEXT("\n"));
+        }
+    } while (Thread32Next(hThreadSnap, &te32));
+
+    CloseHandle(hThreadSnap);
+    return(TRUE);
+}
+
+static int traverse_heap_list(DWORD dw_pid) {
+    HEAPLIST32 hl;
+
+    HANDLE hHeapSnap = CreateToolhelp32Snapshot(TH32CS_SNAPHEAPLIST, dw_pid);
+
+    hl.dwSize = sizeof(HEAPLIST32);
+
+    if (hHeapSnap == INVALID_HANDLE_VALUE)
+    {
+        printf("CreateToolhelp32Snapshot failed (%d)\n", GetLastError());
+        return 1;
+    }
+
+    if (Heap32ListFirst(hHeapSnap, &hl))
+    {
+        do
+        {
+            HEAPENTRY32 he;
+            ZeroMemory(&he, sizeof(HEAPENTRY32));
+            he.dwSize = sizeof(HEAPENTRY32);
+
+            if (Heap32First(&he, dw_pid, hl.th32HeapID))
+            {
+                printf("\nHeap ID: %d\n", hl.th32HeapID);
+                do
+                {
+                    printf("Start address: %p Block size: %d\n", he.dwAddress, he.dwBlockSize);
+
+                    he.dwSize = sizeof(HEAPENTRY32);
+                } while (Heap32Next(&he));
+            }
+            hl.dwSize = sizeof(HEAPLIST32);
+        } while (Heap32ListNext(hHeapSnap, &hl));
+    }
+    else printf("Cannot list first heap (%d)\n", GetLastError());
+
+    CloseHandle(hHeapSnap);
+
+    return 0;
+}
+
+static void print_error(TCHAR const* msg) {
+    DWORD eNum;
+    TCHAR sysMsg[256];
+    TCHAR* p;
+
+    eNum = GetLastError();
+    FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL, eNum,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), // Default language
+        sysMsg, 256, NULL);
+
+    // Trim the end of the line and terminate it with a null
+    p = sysMsg;
+    while ((*p > 31) || (*p == 9))
+        ++p;
+    do { *p-- = 0; } while ((p >= sysMsg) &&
+        ((*p == '.') || (*p < 33)));
+
+    // Display the message
+    _tprintf(TEXT("\n  WARNING: %s failed with error %d (%s)"), msg, eNum, sysMsg);
 }
